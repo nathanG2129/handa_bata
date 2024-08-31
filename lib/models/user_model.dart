60ae267c0c75249e124924cd7801482e8c18a501
nathanG2129 <127573862+nathanG2129@users.noreply.github.com>
2024-08-31 13:22:51 +0800

    Add email to user_model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,17 +1,18 @@
 class UserProfile {
-  String profileId;
-  String nickname;
-  int avatarId;
-  List<int> badgeShowcase;
-  int bannerId;
-  int exp;
-  int expCap;
-  bool hasShownCongrats;
-  int level;
-  int totalBadgeUnlocked;
-  int totalStageCleared;
-  List<int> unlockedBadge;
-  List<int> unlockedBanner;
+  final String profileId;
+  final String nickname;
+  final int avatarId;
+  final List<int> badgeShowcase;
+  final int bannerId;
+  final int exp;
+  final int expCap;
+  final bool hasShownCongrats;
+  final int level;
+  final int totalBadgeUnlocked;
+  final int totalStageCleared;
+  final List<int> unlockedBadge;
+  final List<int> unlockedBanner;
+  final String email; // Add email field
 
   UserProfile({
     required this.profileId,
@@ -27,6 +28,7 @@ class UserProfile {
     required this.totalStageCleared,
     required this.unlockedBadge,
     required this.unlockedBanner,
+    required this.email, // Initialize email field
   });
 
   Map<String, dynamic> toMap() {
@@ -44,6 +46,7 @@ class UserProfile {
       'totalStageCleared': totalStageCleared,
       'unlockedBadge': unlockedBadge,
       'unlockedBanner': unlockedBanner,
+      'email': email, // Include email in the map
     };
   }
 }

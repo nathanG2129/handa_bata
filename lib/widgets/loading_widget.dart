c3effc0c9466041ace9ba47326489214a2f0754a
nathanG2129 <127573862+nathanG2129@users.noreply.github.com>
2024-10-30 16:48:29 +0800

    Edited loading_widget

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -1,30 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoadingWidget extends StatelessWidget {
+class LoadingWidget extends StatefulWidget {
   const LoadingWidget({super.key});
+
+  @override
+  _LoadingWidgetState createState() => _LoadingWidgetState();
+}
+
+class _LoadingWidgetState extends State<LoadingWidget> {
+  final List<String> _loadingFrames = [
+    'Loading',
+    'Loading.',
+    'Loading..',
+    'Loading...'
+  ];
+  int _currentFrame = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 500), (Timer timer) {
+      setState(() {
+        _currentFrame = (_currentFrame + 1) % _loadingFrames.length;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/gifs/Walking.gif',
-            width: 200,
-            height: 200,
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: Image.asset(
+              'assets/gifs/Walking.gif',
+              fit: BoxFit.contain,
+              filterQuality: FilterQuality.none,
+            ),
           ),
-          const SizedBox(height: 2),
-          Text(
-            'Loading...',
-            style: GoogleFonts.rubik(
-              fontSize: 16,
-              color: Colors.white,
+          Transform.translate(
+            offset: const Offset(0, -8), // Adjust the vertical offset as needed
+            child: Text(
+              _loadingFrames[_currentFrame],
+              style: GoogleFonts.rubik(
+                fontSize: 16,
+                color: Colors.white,
+              ),
             ),
           ),
         ],
       ),
     );
   }
-} 
+}

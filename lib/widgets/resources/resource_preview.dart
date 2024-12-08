47b497917e4cc24ddbe70bd846149ebf1c1e6d34
nathanG2129 <127573862+nathanG2129@users.noreply.github.com>
2024-12-09 02:48:57 +0800

    fix: Update SVG icon dimensions to zero for improved layout
    - Changed the width and height of the SVG icon in resource_preview.dart from 24 to 0, effectively hiding the icon.
    - This adjustment is aimed at enhancing the layout and visual presentation of the resource preview component, ensuring a cleaner interface.

--- lib/widgets/resources/resource_preview.dart
+++ lib/widgets/resources/resource_preview.dart
@@ -199,8 +199,8 @@ class _ResourcePreviewState extends State<ResourcePreview> {
                                   d="M13 17V3h-2v10H9v-2H7v2h2v2h2v2h2zm8 2v-4h-2v4H5v-4H3v6h18v-2zm-8-6v2h2v-2h2v-2h-2v2h-2z"
                                   fill="white"/>
                               </svg>''',
-                              width: 24,
-                              height: 24,
+                              width: 0,
+                              height: 0,
                             ),
                           ),
                         ],
